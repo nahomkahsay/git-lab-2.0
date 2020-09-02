7a477ebd1a1de6c0511272b9a75f90384a39b4ca
Nahom Kahsay <nk934918@ohio.edu>
2020-09-02 17:20:32 -0400

    Message

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nahom Kahsay
+ *        Date: 09/02/2020
+ * Description: This code is a oart of Lab #2 for CS 2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
